e58374f6a39f4fe4bd7c74c1a40a568c350b6282
Rafael Silva <rafaelshembek@gmail.com>
2023-11-26 20:36:52 -0300

    bricando um pouco com nextJs

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -24,6 +24,6 @@ export default function SideNav() {
           </button>
         </form>
       </div>
-    </div>
+    </div >
   );
 }
---
+++ app/ui/fonts.ts
@@ -0,0 +1,4 @@
+import { Inter, Lusitana } from "next/font/google";
+
+export const inter = Inter({ subsets: ['latin'] })
+export const teste = Lusitana({ subsets: ['latin'], weight: '400', style: "normal" })
